c695b699e53705eacd187ddf62e249488ed70a76
KimHayun <95-ha@naver.com>
2021-05-31 15:51:54 +0900

    5월31일

--- C++/Chat1/Overload.cpp
+++ C++/Chat1/Overload.cpp
@@ -4,12 +4,12 @@ int Add(int a, int b);
 int Add(int a, int b, int c);
 double Add(double a, double b);
 
-int main() {
-
-	printf("1+2=%d\n", Add(1, 2));
-	printf("3+4+5=%d\n", Add(3, 4, 5));
-	printf("1.414+2.54=%f\n", Add(1.414, 2.54));
-}
+//int main() {
+//
+//	printf("1+2=%d\n", Add(1, 2));
+//	printf("3+4+5=%d\n", Add(3, 4, 5));
+//	printf("1.414+2.54=%f\n", Add(1.414, 2.54));
+//}
 int Add(int a, int b)
 {
 	return a + b;
